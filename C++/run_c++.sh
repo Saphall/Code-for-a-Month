a18b255945c57b8dc9cbc231bbbb61a4d36cac8f
Saphall <safalsakha@gmail.com>
2022-02-21 20:43:16 +0545

    Configure Compilation

---
+++ C++/run_c++.sh
@@ -0,0 +1,44 @@
+# -----------------LINUX------------------- #
+# This file helps to build the C++ programs into 'build/' folder.
+
+# Commands :
+# _______________________________________
+# | chmod +x run_c++.sh (first time only) |
+# | ./run_c++.sh                          |
+# | ./build/FILE_NAME_HERE              |
+# -_____________________________________-
+# eg. To run file `0-hello_world.cpp`, AFTER RUNNING `./run_c++.sh` RUN THE PROGRAM AS `./build/0-hello_world`
+
+# This compiles every C++ files(inside any directory) into `build/` folder.
+# So, after running `./run_c++.sh`, you can run program as:
+# ----------------------------------------
+# >  ./build/FILE_NAME_HERE
+# ----------------------------------------
+#                           - Saphal Shakha
+
+
+#! /bin/bash
+rm -rf build
+mkdir build
+
+for files in `ls *cpp`; do
+    build_file="${files::-4}"
+    g++ $files -o build/$build_file
+done
+
+for folder in `ls`; do
+    if [ $folder == build ]; then
+        echo "Now run command >   ./build/'FILE_NAME_HERE' "
+    else
+        if [ -d $folder ];then
+            cd $folder
+            if [ "$(ls -A .)" ]; then
+                for files in `ls *cpp`; do
+                    g++ $files -o ../build/"${files::-4}"
+                done
+            fi
+            cd ..
+        fi
+    fi
+done
+
